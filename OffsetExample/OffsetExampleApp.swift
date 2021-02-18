7862574dfe5219d02bd3c830b9c2ccc7677998ce
Russell Gordon <rgordon@lcs.on.ca>
2021-02-18 18:08:30 -0500

    Initial Commit

---
+++ OffsetExample/OffsetExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  OffsetExampleApp.swift
+//  OffsetExample
+//
+//  Created by Russell Gordon on 2021-02-18.
+//
+
+import SwiftUI
+
+@main
+struct OffsetExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
